7a1ccb30c2ec724faa453b7151d95138c2d36611
Fede108 <federico.cechich@mi.unc.edu.ar>
2025-01-07 20:04:35 -0300

    recorrer de recursivo a iterativo y uso de pila en crear arbol

--- Segundo_Parcial/src/Main.cpp
+++ Segundo_Parcial/src/Main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 
 #include "../inc/ListaConAbb.h"
+#include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -25,15 +27,17 @@ class ArbolPosicional
 private:
     Nodo* raiz;
     Nodo* ultimo;
+    stack<Nodo*> pila;
     void ArbolPos(NodoAbb *lista, Nodo*& nuevo);
     void imprimir(Nodo* NodoAbb);
 public:
     ArbolPosicional(){
         raiz=new Nodo;
         ultimo = raiz;  
-        raiz->altura = 1;  
+        raiz->altura = 1;
+        pila.push(raiz);  
     };
-    void CreaArbolPos(NodoAbb *lista){ ArbolPos(lista,ultimo);};
+    void CreaArbolPos(NodoAbb *lista){ ArbolPos(lista,pila.top());};
     void ImprimirHojas(){imprimir(raiz);};
     NodoAbb* posicion(int p);
     NodoAbb* recorrer(int bin, Nodo* NodoAbb);
@@ -41,17 +45,17 @@ public:
 
 NodoAbb* ArbolPosicional::recorrer(int bin, Nodo* NodoAbb)
 {
-    if(NodoAbb == NULL) return NULL;
-    if (NodoAbb->altura == 0)
+    while (NodoAbb->altura > 0)
     {
-        return NodoAbb->hoja;
+        int msb = (bin >> (NodoAbb->altura - 1)) & 1;
+        if (msb) { // Si el bit mas significativo es 1
+            NodoAbb = NodoAbb->der; // recorre por el lado derecho 
+        } else { // Si el bit mas significativo es 0
+            NodoAbb = NodoAbb->izq; // recorre por el lado izquierdo
+        }
+        if(NodoAbb == NULL) return NULL;
     }
-    int msb = (bin & (1 << (raiz->altura)-1)); 
-    if (msb) { // Si el bit mas significativo es 1
-        return recorrer(bin << 1, NodoAbb->der); // Desplaza el número a la izq y recorre por el lado derecho
-    } else { // Si el bit mas significativo es 0
-        return recorrer(bin << 1, NodoAbb->izq); // Desplaza el número a la izq y recorre por el lado izquierdo
-    }
+    return NodoAbb->hoja;
 }
 
 NodoAbb* ArbolPosicional :: posicion(int p){
@@ -71,19 +75,19 @@ void ArbolPosicional ::imprimir(Nodo* NodoAbb)
     imprimir(NodoAbb->der);
 }
 
-void ArbolPosicional :: ArbolPos(NodoAbb *lista, Nodo*& nuevo){
+void ArbolPosicional :: ArbolPos(NodoAbb *lista, Nodo*& nuevo){  
     if (nuevo->izq == nullptr)
     {
         if(nuevo->altura == 1){
             nuevo->izq = new Nodo;
             nuevo->izq->altura = nuevo->altura - 1;
             nuevo->izq->hoja = lista;
-            ultimo = nuevo;
             return;
         }
-        else{
+        else{   
             nuevo->izq = new Nodo;
             nuevo->izq->altura = nuevo->altura - 1;
+            pila.push(nuevo->izq);
             ArbolPos(lista, nuevo->izq);
             return;
         }
@@ -94,32 +98,64 @@ void ArbolPosicional :: ArbolPos(NodoAbb *lista, Nodo*& nuevo){
             nuevo->der = new Nodo;
             nuevo->der->altura = nuevo->altura - 1;
             nuevo->der->hoja = lista;
-            ultimo = nuevo;
             return;
         }
         else{
             nuevo->der = new Nodo;
             nuevo->der->altura = nuevo->altura - 1;
+            pila.push(nuevo->der);
             ArbolPos(lista, nuevo->der);
             return;
         }
     }    
-        Nodo *nuevaRaiz = new Nodo; 
-        nuevaRaiz->altura = raiz->altura + 1;
-        nuevaRaiz->izq = raiz;
-        raiz = nuevaRaiz;    
-        ArbolPos(lista,nuevaRaiz);
+
+    pila.pop();
+    if (!pila.empty())
+    {
+        ArbolPos(lista, pila.top());
+        return;
+    }
+    Nodo *nuevaRaiz = new Nodo; 
+    nuevaRaiz->altura = raiz->altura + 1;
+    nuevaRaiz->izq = raiz;
+    raiz = nuevaRaiz;    
+    ArbolPos(lista,nuevaRaiz);
+}
+
+void ordenaSeleccion(ArbolPosicional &P,int N){
+    int i,j,pos,m=0,c=0;
+    Dato* men;
+    for(i=1;i<N;i++){
+
+        men = P.posicion(i)->info; pos=i; m++;  // n-1 movimientos
+
+        for(j=i+1;j<N;j++){  // n-1 veces
+            c++; // n-i-1 comparaciones 
+            if( P.posicion(j)->info->repeticiones < men->repeticiones ){ 
+                men = P.posicion(j)->info; m++; // aprox n/2 veces por iteracion en el peor caso, max a min 
+                pos = j;
+            }
+        }
+
+        P.posicion(pos)->info = P.posicion(i)->info; m++; // 2*(n-1) movimientos
+        P.posicion(i)->info = men; m++;
+
+    }
 }
     
-void add(string palabra, ArbolAVL &T,ArbolPosicional &P) {
+int add(string palabra, ArbolAVL &T,ArbolPosicional &P) {
+  int N = 0;
   T.CreaArbolAVL(palabra);
   if (T.last()->info->repeticiones == 1)
   {
     P.CreaArbolPos(T.last());
+    N++;
   }
+  return N;
 }
 
-void leer_archivo(string archivo_,  ArbolAVL &T, ArbolPosicional &P){
+int leer_archivo(string archivo_,  ArbolAVL &T, ArbolPosicional &P){
+    int N = 0;
     string palabra;
     ifstream archivo(archivo_);
     if (archivo.is_open() ){
@@ -129,19 +165,20 @@ void leer_archivo(string archivo_,  ArbolAVL &T, ArbolPosicional &P){
               palabra += c;  
             } else if (!palabra.empty())
             {    
-                add(palabra, T, P);
+                N += add(palabra, T, P);
                 palabra = ""; 
             }   
         }
         archivo.close();
     }
+    return N;
 }
 
 int main(){
     ArbolAVL T;
     ArbolAVL *T_copy;
     ArbolPosicional P;
-    leer_archivo("data.txt", T, P);
+    int N = leer_archivo("data.txt", T, P);
     T_copy = T.Copy();
     T.VerArbol();
     T.IRD();
@@ -152,5 +189,7 @@ int main(){
     cout << "\n";
     P.ImprimirHojas();
     cout << "\n"<< P.posicion(3)->info->palabra<<endl;
+    ordenaSeleccion(P,N);
+    P.ImprimirHojas();
     cout<<endl;system("PAUSE");
 }
