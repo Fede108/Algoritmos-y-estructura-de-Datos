96cca5a6aa0a8dde9961b3b5d6e6e95998e97541
Fede108 <federico.cechich@mi.unc.edu.ar>
2025-02-02 19:44:41 -0700

    ajustes en expresion json

--- Primer_Parcial/inc/ExpresionJson.h
+++ Primer_Parcial/inc/ExpresionJson.h
@@ -8,7 +8,7 @@ using namespace std;
 class ExpresionJson
 {
 private:
-    Estado* estadoActual;
+    Estado* estadoActual;  // puede almacenar cualquier tipo de estado 
     EntreLlaves entreLlaves;    
     Llave llave;                
     Valor valor;                
@@ -17,12 +17,16 @@ private:
     bool correcto;
 public:
     ExpresionJson() {
+        // cada tipo de expresion tiene un puntero a la expresion jsonAyed
+        entreLlaves.setExpresionJson(this);
+        llave.setExpresionJson(this);
+        valor.setExpresionJson(this);
+      
         estadoActual = &entreLlaves; // estado inicial
-        estadoActual->setExpresionJson(this);
         caracter = 0;
     }
-    void setEstado(Estado* estado);
-    bool leer_archivo(string nombre_archivo);
+    void setEstado(Estado* estado);   // seteo el estado segun el modo siguiente correspondiente
+    bool leer_archivo(string nombre_archivo); 
     void generar_archivo();
    
     Estado* getEntreLlaves() { return &entreLlaves; };
